2bf6da0927177c4e109d5d577e9bb8986af10bbe
Poliarnik <mr.alexey.nezh@gmail.com>
2023-04-10 14:08:56 +0300

    v2.0

--- portativ_testing/Model/portativ.py
+++ portativ_testing/Model/portativ.py
@@ -1,6 +1,7 @@
 from selene import have, command
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
+from time import sleep
 
 class Portativ:
     def __init__(self):
@@ -30,8 +31,75 @@ class Portativ:
     def shure_choose(self):
         s('.show-all').click()
         s('[href="https://portativ.ua/ua/category_841832.html?brand=169841"]').click()
-        #ss('.hide').element_by(have.exact_text('Shure')).click()
+        sleep(3)
         s('#right_filter > article > form > fieldset > div > div.filter-include > ul.filter-menu.class-aktivnoe_shumopodavlenie_anc_1368 > li > label > span > a').click()
+        sleep(2)
         return self
 
+    def first_shure_click(self, heads_name: str):
+        s('/html/body/div[1]/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div[5]/a').click()
+        ss('.product-sku').should(have.exact_texts(heads_name))
+        return self
 
+    def cart_test(self):
+        s('/html/body/div[4]/div[2]/div[2]/div[3]/div[3]/div[2]/div[1]/button').click()
+        s('#modal_close').click()
+        s('[href="javascript:void();"]').click()
+        s('.more-btn').click()
+        s('.less-btn').click().click()
+        return self
+    #
+    def login(self, login_name: str, login_pass: str):
+        s('.login-link').click()
+        s('#login-email').click().type(login_name)
+        s('#login-password').click().type(login_pass)
+        s('.login-button').click()
+        return self
+
+    def personal_data_change (self, test_name: str, main_name: str):
+        s('.header-customer-name').click()
+        ss('.dashboard-link')[0].click()
+        s('.edit-profile-link').click()
+        s('#fullname').click().clear().type(test_name)
+        s('[title="Зберегти зміни"]').click()
+        s('#fullname').should(have.value(test_name))
+        s('#fullname').click().clear().type(main_name)
+        s('[title="Зберегти зміни"]').click()
+        s('#fullname').should(have.value(main_name))
+
+
+    def order_list (self):
+        s('body > div.body-container > div.main-content.two-col-left.container > div.catalog-left-side > div > ul > li:nth-child(2) > a').click()
+        #ss('.orders-history-list >').should(have.exact_texts('У вас ще немає замовлень.'))
+        return self
+    # Как сделать проверку что история заказов пустая? P.S. не смог выбрать селектор на историю заказов получше :(
+
+    def second_item_click(self):
+        ss('.cataloggrid-item-image-block')[1].click()
+        return self
+
+    def add_to_favorite (self):
+        s('.catalogwishlist-add').click()
+        s('[title="Обране"]').click()
+        s('[title="Видалення"]').click()
+
+    def comparing_test (self, search: str):
+        s('.logo-image-link').click()
+        s('[id="search-desktop"]').type(search).press_enter()
+        s('.srch-res-text').click()
+        sleep(2)
+        s('.cataloggrid-item-image-block').click()
+        sleep(2)
+        s('.catalogcompare-add').click()
+        ss('[itemprop = "itemListElement"]')[2].click()
+        ss('.cataloggrid-item-image-block')[1].click()
+        sleep(2)
+        s('.catalogcompare-add').click()
+        sleep(2)
+        s('[title="Порівняння"]').click()
+
+    def profile_exit (self):
+        s('.header-customer-name').click()
+        s('[href="https://portativ.ua/ua/customer/account/logout/"]').click()
+        s('.logo-image-link').click()
+        return self
--- tests/portativ_e2e.py
+++ tests/portativ_e2e.py
@@ -2,15 +2,24 @@ from selene import have, command
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
 from time import sleep
-from portativ_testing.Model import port
+from portativ_e2e_test.portativ_testing.Model import port
 
 def test_portativ():
     port.open()
-    port.search('навушники')
+    port.search('наушники')
     port.headphones_btn()
     port.first_item_click()
     port.headphones_cat()
     port.shure_choose()
+    port.first_shure_click('Код товару: 28335')
+    port.cart_test()
+    port.login('oleksii.nizhynskyi@gmail.com', 'qwertyqwerty')
+    port.personal_data_change('second name', 'test test')
+    port.order_list()
+    port.search('KOSS')
+    port.second_item_click()
+    port.add_to_favorite()
+    # port.comparing_test('Audio-Technica')
 
-
+    port.profile_exit()
     sleep(3)
